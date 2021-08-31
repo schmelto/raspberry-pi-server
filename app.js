17dc59f8a019a048d2b58c5318265d77eabc0db1
schmelto <30869493+schmelto@users.noreply.github.com>
2021-09-01 00:09:10 +0200

    update

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+var express = require("express");
+var app = express();
 
 app.get("/url", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+app.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
