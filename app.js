9508076911bcff79b11b975f2f689e59380659af
schmelto <30869493+schmelto@users.noreply.github.com>
2021-09-01 00:12:01 +0200

    feat: add url route

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var app = express();
 
-app.get("/url", (req, res, next) => {
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/names", (req, res, next) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
