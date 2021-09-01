f83f7b308d404ed7c8e7de51ef0a4f09d4279c45
schmelto <30869493+schmelto@users.noreply.github.com>
2021-09-01 17:02:04 +0200

    feat: add simple swagger documentation

--- server.js
+++ server.js
@@ -2,18 +2,30 @@
 var express = require("express");
 var app = express();
 
-app.get("/", (req, res) => {
+const swaggerUi = require('swagger-ui-express'),
+swaggerDocument = require('./swagger.json');
+
+app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/names", (req, res) => {
+app.get("/users", (_req, res) => {
   res.json(["Tony", "Lisa", "Michael", "Ginger", "Food"]);
 });
 
+
+
 //get info
-app.get("/info", (req, res) => {
+app.get("/info", (_req, res) => {
     res.json({});
 });
+
+//swagger
+app.use(
+  '/api-docs',
+  swaggerUi.serve, 
+  swaggerUi.setup(swaggerDocument)
+);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
