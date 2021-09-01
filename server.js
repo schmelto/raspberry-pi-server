3cd17c9314f9bad5e10c086272924a6eec72f1f0
schmelto <30869493+schmelto@users.noreply.github.com>
2021-09-01 17:31:15 +0200

    feat: add mongoserver js

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 /* eslint-disable no-undef */
-var express = require("express");
-var app = express();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+// set port, listen for requests
+const PORT = process.env.PORT || 3000;
 
 const swaggerUi = require("swagger-ui-express"),
   swaggerDocument = require("./swagger.json");
+
+var corsOptions = {
+  origin: "http://localhost:3000",
+};
+
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (_req, res) => {
   res.send("Hello World!");
@@ -21,6 +35,6 @@ app.get("/info", (_req, res) => {
 //swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
